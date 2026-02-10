428d2513a8449db802f637de496c135a95495330
Uma Dhatri <97341880+umadhatri@users.noreply.github.com>
2026-02-10 21:43:26 +0530

    Remove nested BrowserRouter and fix routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Portfolio from "./pages/Portfolio";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/portfolio">
-        <Routes>
-          <Route path="/" element={<Portfolio />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Portfolio />} />
+      </Routes>
     </div>
   );
 }
