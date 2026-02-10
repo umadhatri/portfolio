76cdcc7047453ec7b7813fe99edf5092f230bc8b
Uma Dhatri <97341880+umadhatri@users.noreply.github.com>
2026-02-10 20:08:48 +0530

    Added basename for browser

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/portfolio">
         <Routes>
           <Route path="/" element={<Portfolio />} />
         </Routes>
